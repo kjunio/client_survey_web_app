3c7ea005c0415229641dfd3707f26cf840608927
Elizabeth Baxter <ejbaxte1@asu.edu>
2019-04-15 22:59:34 -0700

    added web services for submit rating button

--- ScrummyBearsProject2/js/home.js
+++ ScrummyBearsProject2/js/home.js
@@ -111,12 +111,12 @@ function HideCompletedSurveys() {
     })
 }
 
-﻿/*function SurveyNumberStorage(id) {
+﻿function SurveyNumberStorage(id) {
     var surveyid = id.slice(-1);
     sessionStorage.setItem("surveynum", surveyid);
     console.log(sessionStorage.getItem("surveynum"));
     window.open("../html/survey.html", "_self");
-}*/
+}
 
 function FeedbackNumberStorage() {
     sessionStorage.setItem("feedbackType", 'general');
---
+++ ScrummyBearsProject2/rating.js
@@ -0,0 +1,20 @@
+
+function onload(event) {
+var starRating = raterJs( {
+		element:document.querySelector("#rater"), 
+		rateCallback:function rateCallback(rating, done) {
+			this.setRating(rating); 
+			done(); 
+		}, 
+		onHover:function(currentIndex, currentRating) {
+			document.querySelector('#live-rating').textContent = currentIndex; 
+		}, 
+		onLeave:function(currentIndex, currentRating) {
+			document.querySelector('#live-rating').textContent = currentRating; 
+			var rate= currentRating;
+		}
+	}); 
+
+}
+
+window.addEventListener("load", onload, false); 
